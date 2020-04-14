3a70edfd23fdb6eb6c7c7baaf9f215b3e4e43496
sari.hatanaka <s-hatanaka@i-enter.co.jp>
2020-04-14 14:39:34 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by 畑中 彩里 on 2020/04/14.
+//  Copyright © 2020 sari.hatanaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
